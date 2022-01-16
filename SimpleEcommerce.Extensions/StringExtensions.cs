f1ac5bb13d8e08830e105e7dfb901ad75a341925
Samuel Neves Carvalho <samuel96carvalho@gmail.com>
2022-01-16 18:40:10 -0300

    First Commit

---
+++ SimpleEcommerce.Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEcommerce.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string PaddingZerosLeft(this string val, int qtdZeros)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < qtdZeros; i++)
+            {
+                sb.Append("0");
+            }
+            sb.Append(val);
+            return sb.ToString();  
+        }
+    }
+}
